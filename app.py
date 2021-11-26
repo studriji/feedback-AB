089d2a406736eb65fda45369a6cdd5ca442eac3e
studriji <70207363+studriji@users.noreply.github.com>
2021-11-27 02:26:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def predict():
 		if(np.argmax(sentiment) == 0):
 			print("negative")
 			#result = 'This is a negative feedback'
-			return render_template('result.html',prediction=0)
+			return render_template('result.html',result=type(feedback))
 		elif(np.argmax(sentiment) == 1):
 			print("positive")
 			#result = 'This is a positive feedback'
